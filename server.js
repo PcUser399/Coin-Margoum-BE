1001fce0411b95f538f149cdfa76e13190fcc6e3
pcuser399 <turki.mahdi.tn@gmail.com>
2026-06-07 01:10:04 +0100

    Update server.js

--- server.js
+++ server.js
@@ -21,6 +21,7 @@ app.use(session({
   secret: process.env.SESSION_SECRET,
   resave: false,
   saveUninitialized: false,
+  rolling: true,
   cookie: {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
